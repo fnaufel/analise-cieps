9a3b842b5df5d7a0411d3efce7bafae5bda93984
Fernando Naufel <fnaufel@gmail.com>
2024-11-01 14:17:01 -0300

    Conclusão

---
+++ functions.R
@@ -0,0 +1,31 @@
+
+ler_dados <- function(
+  aula = 'dados/aula.csv',
+  jogo = 'dados/jogo.csv',
+  nquestoes1 = 16   # qtde de questões da parte 1
+) {
+
+  # Registros de aula não têm data
+  df_aula <- readr::read_csv(aula) %>%
+    mutate(
+      tipo = 'aula',
+      data = NA_Date_,
+      .before = 1
+    )
+    
+  df_jogo <- readr::read_csv(jogo) %>%
+    mutate(
+      tipo = 'jogo',
+      data = lubridate::mdy(data),
+      .before = 1
+    )
+  
+  df_aula %>%
+    bind_rows(df_jogo) %>%
+    mutate(
+      acertos = acertos_parte1 + acertos_parte2,
+      questoes = nquestoes1 + (100 * acertos_parte2 / pct_parte2),
+      pct = 100 * acertos / questoes
+    )
+  
+}
